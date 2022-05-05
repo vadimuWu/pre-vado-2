20ab096ed87328bd8c6cd7cc65edc090b1334d6b
vadimuWu <vadimka.zaletaev@mail.ru>
2022-05-05 19:51:34 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,9 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserServiceImpl;
+
 
 public class Main {
     private static final User user1 = new User("kto", "ponimaet", (byte) 1);
@@ -10,14 +12,14 @@ public class Main {
     private static final User user4 = new User("oshibkoi", "null", (byte) 4);
 
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
-        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
-        userService.getAllUsers();
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        UserDao userDao = new UserDaoHibernateImpl();
+        userDao.createUsersTable();
+        userDao.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userDao.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userDao.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userDao.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
